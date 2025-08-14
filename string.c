573e60c7662e6d6dc88f0da6f360d9986b81ece7
Abijith R A <Abijithra2004@gmail.com>
2025-08-14 17:08:06 +0530

    Funtion

---
+++ string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    char name[100];
+
+    printf("Enter the name of user:");
+    scanf("%s",name);
+
+    printf("The Entered Name is :%s",name);
+
+    return 0;
+}
---
+++ stringone.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+    int values[3][3],i,j;
+    
+
+    printf("Enter the values:");
+
+    for (i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            scanf("%d",&values[i][j]);
+        }
+    }
+    printf("Entered values are :\n");
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("%d\t",values[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
